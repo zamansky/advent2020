553c5584ceb62db3b6ebb8cc6d6907bac1df6990
Mike Zamansky <zamansky@gmail.com>
2020-12-01 08:17:49 -0500

    cleaned up a bit

--- src/day01.clj
+++ src/day01.clj
@@ -4,20 +4,22 @@
 
 (def data
   (->> 
-   ;;(slurp "data/sample01.dat")
-    (slurp "data/day01.dat")
-
-
-   
-       string/split-lines
+   (slurp "data/sample01.dat")
+   ;; (slurp "data/day01.dat")
+    string/split-lines
        (map u/parse-int)
        ))
 
 (defn checksum [x] (first (filter #(= 2020 (+ % x)) data)))
+
+
 (def part1  (reduce * (filter #(not ( nil? %)) (map  checksum  data ))))
+
+
+
 
 (def twosum  (flatten (map (fn [x] (map #(+ % x) data)) data)))
 
 (def part2-numbers  (set (filter #(not ( nil? %)) (map  checksum  twosum ))))
-(def part2 (reduce * part2-numbers)
+(def part2 (reduce * part2-numbers))
 
