8d65ea2b9643c8b527242669ba0c6b337bd1190d
Mike Zamansky <zamansky@gmail.com>
2020-12-01 08:28:15 -0500

    did it another way

--- src/day01.clj
+++ src/day01.clj
@@ -11,15 +11,15 @@
        ))
 
 (defn checksum [x] (first (filter #(= 2020 (+ % x)) data)))
-
-
 (def part1  (reduce * (filter #(not ( nil? %)) (map  checksum  data ))))
 
 
-
+(def part1-again (reduce * (filter  (fn [x]  (some #(= (- 2020 x) %) data)) data)))
 
 (def twosum  (flatten (map (fn [x] (map #(+ % x) data)) data)))
-
 (def part2-numbers  (set (filter #(not ( nil? %)) (map  checksum  twosum ))))
 (def part2 (reduce * part2-numbers))
 
+(def part2-numbers-again (set  (filter  (fn [x]  (some #(= (- 2020 x) %) twosum)) data)))
+(def part2-again (reduce * part2-numbers-again))
+  
