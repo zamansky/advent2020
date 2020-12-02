6ecaa40dd5879a23f061f2b905dcbb1679361a13
Mike Zamansky <zamansky@gmail.com>
2020-12-02 09:40:07 -0500

    added day2

---
+++ src/day02.clj
@@ -0,0 +1,49 @@
+(ns day02
+  (:require [clojure.string :as string]
+            [utils :as u]
+            [hashp.core]))
+
+
+(def parser #"(\d+)-(\d+) ([a-z]): ([a-z]*)")
+
+(def data
+  (->>
+   ;;(slurp "data/sample02.dat")
+   (slurp "data/day02.dat")
+   string/split-lines
+   (map #(re-find parser %))
+   (map (fn [ [_ min max letter password]]
+          {:min (u/parse-int min) :max (u/parse-int max) :letter letter :password password}
+          ) )
+   ))
+
+(defn check-password-part1 [{:keys [min max letter password ]}]
+  (let [counts (frequencies password)
+        l (first letter) ;; string -> char
+        count (get counts l 0)]
+    (and (>= count min)
+         (<= count max))
+    ))
+
+
+
+(def part1-passwords (map check-password-part1 data))
+(def part1 (count (filter true? passwords)))
+
+(defn check-password-part2 [{:keys [min max letter password ]:as line}]
+  (let [range (subs password (dec min) max)
+        counts (frequencies range)
+        l (first letter)
+        ]
+
+    (and
+     (or
+      (= (get password (dec min)) l)
+      (= (get password (dec max)) l))
+     (not= (get password (dec min)) (get password (dec max)))
+    )))
+
+(def part2-passwords (map check-password-part2 data))
+(def part2 (count (filter true? part2-passwords)))
+; 364
+
