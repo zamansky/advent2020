a08a64cc6c97b6265284151021c9f3a6716406d3
Mike Zamansky <zamansky@gmail.com>
2020-12-09 10:16:24 -0500

    cleande up

--- src/day09.clj
+++ src/day09.clj
@@ -1,1 +1,41 @@
-(require 'clj-refactor)
+(ns day09
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]
+            [utils :as u]))
+
+(def data (->> "day09.dat"
+               io/resource
+               slurp
+               str/split-lines
+               (map read-string)
+            ))
+(def parts (partition 26 1  data))
+
+(defn test [part]
+  (let [target (last part)
+        nums (take 25 part)
+        workings     (for [x nums
+                           y nums
+                           :when (and  (not= x y) (= (+ x y) target))
+                           ]
+                       x)
+        ]
+    (if (empty? workings) target 0)
+))
+
+(def invalid-number  (first (filter #(not= 0 %) (map test parts))))
+
+
+
+(defn test2 [range]
+  (if (= invalid-number (apply + range))
+    (+ (apply min range) (apply max range))
+    nil))
+       
+
+(def part2ans (for [x (range 2 (count data))
+      :when (not (empty? (filter #(not (nil? %)) (map test2 ( partition x 1 data)))))
+      ]
+(filter #(not (nil? %)) (map test2 ( partition x 1 data)))
+))
+
