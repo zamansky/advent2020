3d258f3ba5b1ec9d52fae453d54dbd8a35487cc3
Mike Zamansky <zamansky@gmail.com>
2020-12-14 12:57:25 -0500

    added day14

--- src/day14.clj
+++ src/day14.clj
@@ -1,67 +1,95 @@
 (ns day14
   (:require [clojure.java.io :as io]
-            [clojure.string :as str]
-            [hashp.core]))
+            [clojure.string :as str]))
 
-(defn parse-instr [[cmd param]]
-  (if (= "mask" cmd)
+(defn parse-instr [ [cmd param]]
+  (if (= cmd "mask")
     {:instr :mask :param param}
-    (let [ [_ dst] (re-find #"mem\[(\d+)\]" cmd)]
-      {:instr :mem :param param :dst dst}
-  )))
+    (let [ [_ dst] (re-find #"mem\[(\d+)\]" cmd)  ]
+    {:instr :mem :param param :dst dst}
+    )))
 
 (def data (->> "day14.dat"
                io/resource
-
                slurp
                str/split-lines
-               (map  #(re-find #"(.+) = (.+)" %))
+               (map #(re-find #"(.+) = (.+)" %) )
                (map #(drop 1 %))
                (map parse-instr)
                ))
 
-(def state {:mem {} :and-mask 0 :or-mask 0})
-
-
-
 (defn make-and-mask [s]
-  (->>
-   (map {\X \1 \1 \1 \0 \0} s)
+  (->> s
+   (map {\X \1 \1 \1 \0 \0})
    (apply str "2r")
    read-string
    ))
 
 (defn make-or-mask [s]
-  (->>
-  (map {\X \0 \1 \1 \0 \0} s)
-  (apply str "2r")
-  read-string))
+  (->> s
+   (map {\X \0 \1 \1 \0 \0})
+   (apply str "2r")
+   read-string
+   ))
 
 
-(def m "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X")
-(bit-or (make-and-mask m) 23)
-
-;; replace x with 0
-;; 0 or 1 = 1
-;; 0 or = 0 so theys stay the same
-;; 1 will turn on
-(defn mask2-or [s] (map {\1 \1 \x \1 \0 \0} s))
-
-(defn process-cmd [{:keys [and-mask or-mask] :as state}
-                   {:keys [instr dst param] :as instr}]
+(def state {:mem {} :and-mask 0  :or-mask 0 :mask ""})
+(defn part1-process-instr [{:keys [and-mask or-mask] :as state}
+                           {:keys [instr dst param] :as instr}]
   (cond
-    (= instr :mask) (assoc state :and-mask #p (make-and-mask param)
+    (= instr :mask) (assoc state
+                           :and-mask (make-and-mask param)
                            :or-mask (make-or-mask param))
-    (= instr :mem) (assoc-in state [:mem   dst]
-                             (-> param
-                                   read-string
-                                   (bit-or or-mask)
-                                   (bit-and and-mask)
-                                 ))
-    )
-  )
+    (= instr :mem)  (assoc-in state [:mem dst]
+                              (-> param
+                                  read-string
+                                  (bit-or or-mask)
+                                  (bit-and and-mask)))
+    ))
 
-(def init-machine (reduce process-cmd state data))
 
-(def mem (:mem init-machine))
-(def part1-ans) (apply +  (map second mem))
+(def part1-machine (reduce part1-process-instr state data))
+(def mem1 (:mem part1-machine))
+(def part1-ans (apply + (map second mem1)))
+
+(defn build-nums [s]
+  (if (some #{\X} s)
+    (let [s1 (str/replace-first s \X 1)
+          s2 (str/replace-first s \X 0)
+          ]
+      (conj [] (build-nums s1) (build-nums s2))
+      )s))
+
+
+(defn dst->binary-string [dst]
+  (let [s (Integer/toBinaryString (read-string dst))
+        s2 (str (apply str (repeat 36 \0)) s)
+        l (count s2)
+        result (subs s2 (- l 36))
+        ]
+    result))
+
+(defn apply-mask [mem mask]
+  (->>
+   (map (fn [mem-item mask-item]
+          (cond
+            (= mask-item \0) mem-item
+            (= mask-item \1) \1
+            :else \X
+            )) mem mask)
+   (apply str)))
+
+(defn part2-process-instr [{:keys [mask] :as state}
+                           {:keys [instr dst param] :as instr}]
+  (cond
+    (= instr :mask) (assoc state :mask param)
+    (= instr :mem)  (assoc-in state [:mem dst]
+                              (-> param
+                                  read-string
+                                  (bit-or or-mask)
+                                  (bit-and and-mask)))
+    ))
+
+
+
+  
